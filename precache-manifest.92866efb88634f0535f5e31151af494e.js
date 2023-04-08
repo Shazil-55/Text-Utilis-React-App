30d2958299ded6ecac5c674b3c2b64c1a9c9649b
Shazil-55 <shazilnisar7@gmail.com>
2023-04-08 21:41:03 +0500

    text utils app

---
+++ precache-manifest.92866efb88634f0535f5e31151af494e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ce7348b6825512b6945",
+    "url": "/static/css/main.ef2c4693.chunk.css"
+  },
+  {
+    "revision": "1ce7348b6825512b6945",
+    "url": "/static/js/main.1ce7348b.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "7b1b2c2dc45096c8ad66",
+    "url": "/static/js/2.7b1b2c2d.chunk.js"
+  },
+  {
+    "revision": "b117a03f136dce80bebe",
+    "url": "/static/js/runtime~main.b117a03f.js"
+  },
+  {
+    "revision": "bf1fce3cea073421a31228ad5d531a1b",
+    "url": "/index.html"
+  }
+];
